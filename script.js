aa8e561f6a598a6e567d6ceba2f1202f86b16c24
Ak1m01 <107209220+Ak1m01@users.noreply.github.com>
2022-12-20 15:58:27 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+function toggleText() {
+
+    var showMoreText = document.getElementById("moretext");
+
+    if (showMoreText.style.display == "none") {
+        showMoreText.style.display = "inline";
+    } else {
+
+        showMoreText.style.display = "none"
+    }
+}
+function toggleText2() {
+
+    var showMoreText =
+        document.getElementById("moretext2");
+
+    if (showMoreText.style.display == "none") {
+
+
+        showMoreText.style.display = "inline";
+
+    }
+
+    else {
+
+        showMoreText.style.display = "none"
+    }
+}
+function toggleText3() {
+
+    var showMoreText =
+        document.getElementById("moretext3");
+
+    if (showMoreText.style.display == "none") {
+
+
+        showMoreText.style.display = "inline";
+
+    }
+
+    else {
+
+        showMoreText.style.display = "none"
+    }
+}
